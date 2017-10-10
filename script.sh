d9b719a9ce4f4d687c19a6053c93195c7e42d8a4
kesylo <32457792+kesylo@users.noreply.github.com>
2017-10-10 14:40:22 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# install vlc
 sudo apt-get -y install vlc
+
+# Change wallpaper
+sudo gsettings set org.gnome.desktop.background picture-uri "https://raw.githubusercontent.com/kesylo/preseed/master/Sans%20titre.jpg"
+
