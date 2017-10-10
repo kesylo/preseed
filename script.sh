db0e6ccfb68b40a7ff966a39ac086920c36473a7
kesylo <32457792+kesylo@users.noreply.github.com>
2017-10-10 15:08:13 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,12 @@
 sudo apt-get -y install vlc
 
 # Change wallpaper
-sudo gsettings set org.gnome.desktop.background picture-uri "https://raw.githubusercontent.com/kesylo/preseed/master/Sans%20titre.jpg"
+sudo gsettings set org.gnome.desktop.background picture-uri "https://raw.githubusercontent.com/kesylo/preseed/master/famocoWallpaper.jpg"
 
+#other
+sudo wget  -O /tmp/famocoWallpaper.jpg "https://raw.githubusercontent.com/kesylo/preseed/master/famocoWallpaper.jpg"
+sudo gsettings set org.gnome.desktop.background picture-uri file:////tmp/famocoWallpaper.jpg
+
+
+sudo gconftool-2 --type=string --set /desktop/gnome/background/picture_filename /tmp/famocoWallpaper.jpg
+sudo gconftool-2 --type=string --set /desktop/gnome/background/picture_options stretched    # stretch the image
