7b569b50b8160ea13ba86ff050bb287c1b7f2abf
kesylo <32457792+kesylo@users.noreply.github.com>
2017-10-10 15:09:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,3 +13,9 @@ sudo gsettings set org.gnome.desktop.background picture-uri file:////tmp/famocoW
 
 sudo gconftool-2 --type=string --set /desktop/gnome/background/picture_filename /tmp/famocoWallpaper.jpg
 sudo gconftool-2 --type=string --set /desktop/gnome/background/picture_options stretched    # stretch the image
+
+#Chrome
+cd /tmp
+sudo apt-get -y install gdebi
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi -n google-chrome-stable_current_amd64.deb
