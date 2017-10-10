192574d183824f6df9e7dc3b14babd4afa4d96eb
kesylo <32457792+kesylo@users.noreply.github.com>
2017-10-10 15:41:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,3 +19,26 @@ cd /tmp
 sudo apt-get -y install gdebi
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi -n google-chrome-stable_current_amd64.deb
+
+#Slack
+cd /tmp/
+wget "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.1-amd64.deb"
+sudo apt-get install ./slack-desktop-2.8.1-amd64.deb
+
+#Skype
+sudo wget https://repo.skype.com/latest/skypeforlinux-64.deb
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo dpkg -i skypeforlinux-64.deb
+
+#Open office
+cd /tmp
+sudo apt-get -y remove libreoffice* openoffice*
+sudo apt-get -y purge -f libreoffice*
+sudo apt-get -y autoremove
+sudo wget sourceforge.net/projects/openofficeorg.mirror/files/4.1.1/binaries/en-GB/Apache_OpenOffice_4.1.1_Linux_x86-64_install-deb_en-GB.tar.gz
+sudo tar -xzvf Apache_OpenOffice_4.1.1_Linux_x86-64_install-deb_en-GB.tar.gz
+cd en-GB/DEBS
+sudo dpkg -i *.deb
+cd desktop-integration
+sudo dpkg -i *.deb
